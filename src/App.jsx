323c514440679cefca6470450e13660403a9ece8
Brandon Ringe <brandon.ringe@gmail.com>
2017-06-18 15:11:00 -0500

    Remove material ui references and package, add semantic ui react components and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import SlackMessage from './components/SlackMessage';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
 import { OrderedSet } from 'immutable';
-
-// Needed for onTouchTap 
-// http://stackoverflow.com/a/34015469/988941 
-injectTapEventPlugin();
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
 
   constructor() {
     super();
 
-    this.newsRetrievalWorker = new SharedWorker(`${process.env.PUBLIC_URL}/news-retrieval-worker.js`);
-
     this.state = {
       newsItems: OrderedSet()
     };
 
-    this.newsRetrievalWorker.port.onmessage = this.handleNewsWorkerMessage.bind(this);
+    // this.newsRetrievalWorker = new SharedWorker(`${process.env.PUBLIC_URL}/news-retrieval-worker.js`);
+    // this.newsRetrievalWorker.port.onmessage = this.handleNewsWorkerMessage.bind(this);
   }
 
   handleNewsWorkerMessage(event) {
@@ -37,21 +29,11 @@ class App extends Component {
   render() {
     console.log('render called');
     return (
-      <MuiThemeProvider>
         <div>
-          <AppBar
-            title="Decent News"
-            showMenuIconButton={false}
-          />
-          {this.state.newsItems.map(message => {
-            return (
-              <SlackMessage
-                key={message.id}
-                message={message} />
-            );
-          })}
+          <Button primary>
+            Click Here
+          </Button>
         </div>
-      </MuiThemeProvider>
     );
   }
 }
